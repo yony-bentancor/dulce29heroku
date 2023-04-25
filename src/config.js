07c50a7be5d2fa9ce951d617b7bfe768c72a1188
yony-bentancor <yonybentancor@gmail.com>
2023-04-25 11:45:42 -0300

    se cambio variables

--- src/config.js
+++ src/config.js
@@ -1,15 +1,22 @@
-const CLAVE_SECRETA = process.env.CLAVE_SECRETA || "lunes23deagosto";
+const CLAVE_SECRETA =
+  process.env.CLAVE_SECRETA || "dulce29"; /* "lunes23deagosto"; */
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80; /* 3001 */
 
 const DB_CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://localhost/proyecto";
+  process.env.DB_CONNECTION_STRING ||
+  "mongodb+srv://proyectodulce:dulce29@cluster0.bi9aze0.mongodb.net/?retryWrites=true&w=majority"; /* "mongodb://localhost/proyecto" */
 
-const HOST = "http://localhost";
+const uri =
+  "mongodb+srv://proyectodulce:dulce29@cluster0.bi9aze0.mongodb.net/?retryWrites=true&w=majority";
 
+const HOST = "https://dulce29.herokuapp.com/";
+/* "http://localhost";
+ */
 module.exports = {
   CLAVE_SECRETA,
   PORT,
   DB_CONNECTION_STRING,
   HOST,
+  uri,
 };
