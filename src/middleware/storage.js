a0c1732c34d9afd90b8c198947979bcac9100fe1
yony-bentancor <yonybentancor@gmail.com>
2023-09-25 10:42:03 -0300

    storage

--- src/middleware/storage.js
+++ src/middleware/storage.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/uploads");
+    cb(null, "../main/src/public/uploads");
   },
   filename: function (req, file, cb) {
     cb(null, `${file.fieldname}-${Date.now()}.jpg`);
