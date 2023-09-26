7ae6fef5ac232e0425088066f3f5e08d929186d2
yony-bentancor <yonybentancor@gmail.com>
2023-09-26 13:39:04 -0300

    ultimo refresh

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,8 +1,8 @@
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-  accessKeyId: "yonybentancor",
-  secretAccessKey: "Dinamitadog2023",
+  accessKeyId: "AKIAY6K62BUG4GBUYHWG",
+  secretAccessKey: "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",
   region: "us-east-2", // Por ejemplo, 'us-east-1'
 });
 
