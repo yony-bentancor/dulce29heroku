6b03b83004f5a520c4f05543baf94e572ef88f65
yony-bentancor <yonybentancor@gmail.com>
2023-09-27 10:06:21 -0300

    cambio zona

--- src/aws-config.js
+++ src/aws-config.js
@@ -3,7 +3,7 @@ const AWS = require("aws-sdk");
 AWS.config.update({
   accessKeyId: "AKIAY6K62BUG4GBUYHWG",
   secretAccessKey: "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",
-  region: "us-east-2", // Por ejemplo, 'us-east-1'
+  region: "us-east-1", // Por ejemplo, 'us-east-1'
 });
 
 const s3 = new AWC.S3();
