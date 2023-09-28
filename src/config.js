961f48c5304e047dea96648dde4d91f0ea84d7e9
yony-bentancor <yonybentancor@gmail.com>
2023-09-28 13:53:30 -0300

    modified:   .Procfile #	modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,11 @@ const CLAVE_SECRETA =
 
 const PORT = process.env.PORT || 80; /*3001 */
 
+const TU_ACCESS_KEY_ID = process.env.TU_ACCESS_KEY_ID || "AKIAY6K62BUGQIZCBAGX";
+const TU_SECRET_ACCESS_KEY =
+  process.env.TU_SECRET_ACCESS_KEY ||
+  "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";
+
 const DB_CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING ||
   "mongodb+srv://proyectodulce:dulce29@cluster0.bi9aze0.mongodb.net/?retryWrites=true&w=majority"; /* "mongodb://localhost/proyecto" */
@@ -17,6 +22,8 @@ const HOST = "https://dulce29.herokuapp.com/";
 module.exports = {
   CLAVE_SECRETA,
   PORT,
+  TU_SECRET_ACCESS_KEY,
+  TU_ACCESS_KEY_ID,
   DB_CONNECTION_STRING,
   HOST,
   uri,
