c12d05183a2585f44de984ad4884a901f4e34e02
yony-bentancor <yonybentancor@gmail.com>
2023-09-28 22:53:00 -0300

    modified:   src/awsConfig.js #	modified:   src/utils/s3.js

--- src/awsConfig.js
+++ src/awsConfig.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "us-east-1", // Cambia a tu región AWS
+  region: "us-east-2", // Cambia a tu región AWS
 });
 
 const s3 = new AWS.S3();
--- src/utils/s3.js
+++ src/utils/s3.js
@@ -4,7 +4,6 @@ const s3 = new aws.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION, // Agrega la región de tu bucket de S3
-  Bucket: process.env.AWS_BUCKET_NAME,
 });
 
 const uploadToS3 = (file, key) => {
