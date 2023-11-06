c57a324d460722dc6ed675adc62070616e1e7ca8
yony-bentancor <yonybentancor@gmail.com>
2023-11-06 18:37:01 -0300

    server

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const pageRoutes = require("./routes/page.routes");
 const posteoRoutes = require("./routes/posteo.routers");
 const userRoutes = require("./routes/user.routes");
 const nodemailer = require("nodemailer");
-require("./tareaProgramada");
+require("tareaProgramada");
 
 nunjucks.configure("./src/views", {
   autoescape: true,
