c4cbc1a6d75e6004e067e6acc39255cf79fedd1d
yony-bentancor <yonybentancor@gmail.com>
2023-11-07 15:02:32 -0300

    TpROGRAMADA

--- src/tareaProgramada.js
+++ src/tareaProgramada.js
@@ -11,18 +11,13 @@ const tareaProgramada = schedule.scheduleJob("*/3 * * * *", async function () {
     const limiteDias = 1;
 
     // Busca usuarios con repitePedido establecido en true
-
-    const ultimoPedido = await Pedido.findOne()
-      .sort({ Numero_pedido: -1 })
-      .select("Numero_pedido");
-
-    // Incrementar el número de pedido
-    const Numero_pedido = ultimoPedido ? ultimoPedido.Numero_pedido + 1 : 1;
-
     const usuariosRepetidores = await User.find({ repitePedido: true }).exec();
 
     for (const usuario of usuariosRepetidores) {
-      // Busca el último pedido correspondiente al cliente
+      // Obtener el intervalo de repetición personalizado del usuario (en minutos)
+      const intervaloRepetición = usuario.intervaloRepetición;
+
+      // Buscar el último pedido correspondiente al cliente
       const ultimoPedido = await Pedido.findOne({
         username: usuario.username,
       })
@@ -31,9 +26,9 @@ const tareaProgramada = schedule.scheduleJob("*/3 * * * *", async function () {
 
       if (ultimoPedido) {
         const tiempoTranscurrido = hoy - ultimoPedido.createdAt;
-        const diasTranscurridos = tiempoTranscurrido / (1000 * 60 * 60 * 24);
+        const minutosTranscurridos = tiempoTranscurrido / (1000 * 60);
 
-        if (diasTranscurridos >= limiteDias) {
+        if (minutosTranscurridos >= intervaloRepetición) {
           // Crea un nuevo pedido con los mismos productos que el último pedido
           const nuevoPedido = new Pedido({
             username: usuario.username,
@@ -41,15 +36,7 @@ const tareaProgramada = schedule.scheduleJob("*/3 * * * *", async function () {
             telefono: usuario.telefono,
             Numero_pedido: Numero_pedido,
             Estado: "Pendiente",
-
-            // Otros campos del pedido como Estado, Pago, Monto_total, etc.
-            createdAt: new Date(), // Actualiza la fecha de creación
-            productos: ultimoPedido.productos, // Usa los mismos productos
-            Monto_total: ultimoPedido.Monto_total,
-            Costo_total: ultimoPedido.Costo_total,
-            Pago: ultimoPedido.Pago,
-            Mes: ultimoPedido.Mes,
-            Descuento: ultimoPedido.Descuento,
+            // ... (resto de los campos del pedido)
           });
 
           await nuevoPedido.save(); // Guarda el nuevo pedido en la base de datos
